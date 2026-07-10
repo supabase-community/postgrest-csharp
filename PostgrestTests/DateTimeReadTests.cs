45567be73de83ec2b8a706d6c97cd8d91866d671
Guillaume Faas <59444272+Tr00d@users.noreply.github.com>
2026-07-10 10:29:56 +0200

    test: add other non-UTC scenario on DateTimeReadTests (#125)

--- PostgrestTests/DateTimeReadTests.cs
+++ PostgrestTests/DateTimeReadTests.cs
@@ -5,41 +5,44 @@ using Newtonsoft.Json;
 using Supabase.Postgrest;
 using PostgrestTests.Models;
 
-namespace PostgrestTests
+namespace PostgrestTests;
+
+[TestClass]
+public class DateTimeReadTests
 {
-    [TestClass]
-    public class DateTimeReadTests
+    private static KitchenSink Deserialize(string json) =>
+        JsonConvert.DeserializeObject<KitchenSink>(json, Client.SerializerSettings())!;
+
+    private static DateTime ParseUtc(string wireValue) =>
+        DateTimeOffset.Parse(wireValue, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind).UtcDateTime;
+
+    [DataTestMethod]
+    [DataRow("2024-06-26T18:30:45.1234560+00:00", DisplayName = "UTC (+00:00)")]
+    [DataRow("2024-06-26T20:30:45.1234560+02:00", DisplayName = "+02:00")]
+    public void GivenTimestamptzWithSubSeconds_ShouldPreserveInstantAndPrecisionOnRead(string wireValue)
     {
-        private const string WireValue = "2024-06-26T18:30:45.1234560+00:00";
+        var model = Deserialize($"{{\"datetime_value\":\"{wireValue}\"}}");
+        Assert.IsTrue(model.DateTimeValue.HasValue);
+        Assert.AreEqual(ParseUtc(wireValue), model.DateTimeValue!.Value.ToUniversalTime());
+    }
 
-        private static readonly DateTime ExpectedInstant =
-            DateTimeOffset.Parse(WireValue, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind).UtcDateTime;
+    [DataTestMethod]
+    [DataRow("2024-06-26T18:30:45.1234560+00:00", DisplayName = "UTC (+00:00)")]
+    [DataRow("2024-06-26T20:30:45.1234560+02:00", DisplayName = "+02:00")]
+    public void GivenTimestamptz_ShouldNotReturnUnspecifiedKindOnRead(string wireValue)
+    {
+        var model = Deserialize($"{{\"datetime_value\":\"{wireValue}\"}}");
+        Assert.AreNotEqual(DateTimeKind.Unspecified, model.DateTimeValue!.Value.Kind);
+    }
 
-        private static KitchenSink Deserialize(string json) =>
-            JsonConvert.DeserializeObject<KitchenSink>(json, Client.SerializerSettings())!;
-
-        [TestMethod("DateTime read: preserves the instant and sub-second precision of a timestamptz")]
-        public void GivenTimestamptzWithSubSeconds_ShouldPreserveInstantAndPrecisionOnRead()
-        {
-            var model = Deserialize($"{{\"datetime_value\":\"{WireValue}\"}}");
-            Assert.IsTrue(model.DateTimeValue.HasValue);
-            Assert.AreEqual(ExpectedInstant, model.DateTimeValue!.Value.ToUniversalTime());
-        }
-
-        [TestMethod("DateTime read: does not strip the kind of a timestamptz to Unspecified")]
-        public void GivenTimestamptz_ShouldNotReturnUnspecifiedKindOnRead()
-        {
-            var model = Deserialize($"{{\"datetime_value\":\"{WireValue}\"}}");
-            Assert.AreNotEqual(DateTimeKind.Unspecified, model.DateTimeValue!.Value.Kind);
-        }
-
-        [TestMethod("DateTime read: preserves each instant of a timestamptz array")]
-        public void GivenTimestamptzList_ShouldPreserveEachInstantOnRead()
-        {
-            var model = Deserialize($"{{\"list_of_datetimes\":[\"{WireValue}\"]}}");
-            Assert.IsNotNull(model.ListOfDateTimes);
-            Assert.AreEqual(1, model.ListOfDateTimes!.Count);
-            Assert.AreEqual(ExpectedInstant, model.ListOfDateTimes[0].ToUniversalTime());
-        }
+    [DataTestMethod]
+    [DataRow("2024-06-26T18:30:45.1234560+00:00", DisplayName = "UTC (+00:00)")]
+    [DataRow("2024-06-26T20:30:45.1234560+02:00", DisplayName = "+02:00")]
+    public void GivenTimestamptzList_ShouldPreserveEachInstantOnRead(string wireValue)
+    {
+        var model = Deserialize($"{{\"list_of_datetimes\":[\"{wireValue}\"]}}");
+        Assert.IsNotNull(model.ListOfDateTimes);
+        Assert.AreEqual(1, model.ListOfDateTimes!.Count);
+        Assert.AreEqual(ParseUtc(wireValue), model.ListOfDateTimes[0].ToUniversalTime());
     }
 }
